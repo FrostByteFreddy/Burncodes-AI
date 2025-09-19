ea354e7e2a68150ac7c9b2706374e956e0533d0c
Moritz Burn <moritzburn@Moritzs-MacBook-Air.local>
2025-09-19 16:51:25 +0200

    Bare-minimum browserConfig to see if chrome even starts..

--- backend/app/data_processing/crawler.py
+++ backend/app/data_processing/crawler.py
@@ -5,11 +5,6 @@ CRAWL_CACHE_PATH = os.getenv("CRAWL_CACHE_PATH", "/data/crawl4ai_cache")
 
 browser_config = BrowserConfig(
     headless=True,
-    verbose=False,
-    user_agent_mode="random",
-    viewport_width=1920,
-    viewport_height=1080,
-    extra_args=['--disable-crash-reporter']
 )
 
 shared_crawler = AsyncWebCrawler(
