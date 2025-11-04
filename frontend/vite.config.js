5348eef1ce1b6718f5bff64c8791f115d7e98a01
MoritzBurn <66977264+FrostByteFreddy@users.noreply.github.com>
2025-11-04 09:23:07 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    isDevelopment && vueDevTools(), // Prevents build error on render (because no browser API exists)
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === 'development'
+
+  return {
+    plugins: [
+      vue(),
+      isDevelopment && vueDevTools(), // Prevents build error on render (because no browser API exists)
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
 })
